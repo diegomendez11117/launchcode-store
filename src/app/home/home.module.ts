333eb587c884808167f31dd94b1204622a1cc016
Enrique <diegomendez11117@gmail.com>
2021-05-24 18:08:37 -0700

    Deploy correct

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BannerComponent } from './components/banner/banner.component';
+
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
@@ -9,7 +9,6 @@ import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   declarations: [
-    BannerComponent,
     HomeComponent
   ],
   imports: [
